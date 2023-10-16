3fa9aea732fe7a467fcc9b482553778d7cd7c33a
Jardel Machado <dell_machado@yahoo.com.br>
2023-10-16 00:20:21 -0300

    feat: moved filter and card list to get unit service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Location } from 'src/app/models/location.interface';
+import { GetUnitsService } from 'src/app/services/get-units.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'desafio-smarfit';
+  
+  showList = new BehaviorSubject(false);
+  unitsList: Location[] = [];
+
+  constructor(private unitService: GetUnitsService) {}
+
+  onSubmit() {        
+    this.unitsList = this.unitService.getFilteredUnits();
+    this.showList.next(true);
+  }
 }
--- src/app/services/get-units.service.ts
+++ src/app/services/get-units.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { UnitsResponse } from '../models/units-response.interface';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Location } from 'src/app/models/location.interface';
+import { UnitsResponse } from 'src/app/models/units-response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
+  
 export class GetUnitsService {
-
   readonly apiUrl = "https://test-frontend-developer.s3.amazonaws.com/data/locations.json";
 
-  constructor(private httpClient: HttpClient) { }
+  private allUnitsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([]);
+  private allUnits$: Observable<Location[]> = this.allUnitsSubject.asObservable();
+  private filteredUnits: Location[] = [];
 
-  getAllUnits(): Observable<UnitsResponse> { 
-    return this.httpClient.get<UnitsResponse>(this.apiUrl)
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<UnitsResponse>(this.apiUrl).subscribe(data => {
+      this.allUnitsSubject.next(data.locations);
+      this.filteredUnits = data.locations;
+    });
+  }
+
+  getAllUnits(): Observable<Location[]> {
+    return this.allUnits$;
+  }
+
+  getFilteredUnits() {
+    return this.filteredUnits;
+  }
+
+  setFilteredUnits(value: Location[]) {
+    this.filteredUnits = value;
   }
 }
